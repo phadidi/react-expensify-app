e2e5a8b988d37ada4f4767b4980903ce23a5fa1f
phadidi <hadidip@hotmail.com>
2021-01-07 12:53:46 -0800

    added test case

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListItem } from '../../components/ExpenseListItem';
 import defaultExpenses from '../fixtures/defaultExpenses';
+
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem {...defaultExpenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
