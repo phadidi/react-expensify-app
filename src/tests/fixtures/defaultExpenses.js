b79310115c46d6e9fc03e00b11f5b188e4e81197
phadidi <hadidip@hotmail.com>
2021-01-07 14:25:23 -0800

    added ExpensesSummary component and tests

--- src/tests/fixtures/defaultExpenses.js
+++ src/tests/fixtures/defaultExpenses.js
@@ -1,17 +1,17 @@
 import moment from 'moment';
 export default [
-  { id: '1', description: 'PS5', amount: 500, createdAt: moment(1) },
+  { id: '1', description: 'PS5', amount: 50000, createdAt: moment(1) },
   {
     id: '2',
     description: 'PS3 Repairs',
-    amount: 270,
+    amount: 27050,
     note: 'cleaned out stray plastic bits, refreshed aging HDD',
     createdAt: moment(2).subtract(1, 'days').valueOf(),
   },
   {
     id: '3',
     description: 'PS2 Repairs',
-    amount: 200,
+    amount: 20025,
     note: 'removed dust from fan, covered all open ports',
     createdAt: moment(2),
   },
